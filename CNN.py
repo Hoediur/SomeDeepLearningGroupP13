53ab0b3b40e8e2217c368a1bed2ac9cee9b1af85
SS-PC\Simon <simonstilz@googlemail.com>
2017-02-02 15:45:09 +0100

    initial working CNN

--- CNN.py
+++ CNN.py
@@ -7,7 +7,7 @@ import lasagne.layers as L
 import numpy as np
 import string
 import WordMatrix
-
+theano.config.compute_test_value = "warn"
 char_vocab = unicode(string.ascii_letters + string.punctuation + string.digits+"äöüÄÖÜß", "utf-8") #define vocabulary for characters
 char_index_lookup = dict()
 
@@ -26,37 +26,61 @@ def create_char_embedding_matrix():
     return embedding_matrix
 
 def build_cnn():
-    data_size = (None,1,None,101)  # Batch size x Img Channels x Height x Width
+    data_size = (None,10,100)  # Batch size x Img Channels x Height x Width
 
-    input_var = T.tensor4(name = "input",dtype='int32')
+    input_var = T.tensor3(name = "input",dtype='int64')
 
+    values = np.array(np.random.randint(0,1,(5,10,100)))
+    input_var.tag.test_value = values
     input_layer = L.InputLayer(data_size, input_var=input_var)
 
     W = create_char_embedding_matrix()
 
     embed_layer = L.EmbeddingLayer(input_layer, input_size=102,output_size=101, W=W)
 
-    #conv_layer_1 = L.Conv2DLayer(embed_layer, 4, (1,101), 1, 0)
+
+    reshape = L.reshape(embed_layer,(-1,100,101))
+    dim_shuffle = L.dimshuffle(reshape,(0,2,1))
+    #conv_layer_1 = L.Conv2DLayer(embed_layer, 4, (1), 1, 0)
     #pool_layer_1 = L.MaxPool1DLayer(conv_layer_1, pool_size=1)
+    print L.get_output(dim_shuffle).tag.test_value.shape
 
-    conv_layer_1 = L.Conv2DLayer(embed_layer, 4, (2,101), 1, 1)
-    pool_layer_1 = L.MaxPool1DLayer(conv_layer_1, pool_size=2)
-
-    conv_layer_2 = L.Conv2DLayer(embed_layer, 4, (3,101), 1, 2)
-    pool_layer_2 = L.MaxPool1DLayer(conv_layer_2, pool_size=3)
-
-    merge_layer = L.ConcatLayer([pool_layer_1, pool_layer_2], 0)
+    conv_layer_1 = L.Conv1DLayer(dim_shuffle, 50, 2, 1)
 
 
+    print L.get_output(conv_layer_1).tag.test_value.shape
+    print "TEST"
+    pool_layer_1 = L.MaxPool1DLayer(conv_layer_1, pool_size=99)
+    print L.get_output(pool_layer_1).tag.test_value.shape
+    reshape_conv_1 = L.reshape(pool_layer_1,(-1,50))
 
-    x = input_var
-    #x = T.tensor4(name = "testname",dtype='int32')
+    conv_layer_2 = L.Conv1DLayer(dim_shuffle, 50, 3, 1)
+    pool_layer_2 = L.MaxPool1DLayer(conv_layer_2, pool_size=98)
+    reshape_conv_2 = L.reshape(pool_layer_2, (-1, 50))
+
+    merge_layer = L.ConcatLayer([reshape_conv_1, reshape_conv_2], 1)
+    print L.get_output(merge_layer).tag.test_value.shape
+    reshape_output = L.reshape(merge_layer,(-1,10,100))
+    print L.get_output(reshape_output).tag.test_value.shape
+
+    x = T.tensor3(name = "testname",dtype='int32')
     #x = T.imatrix()
-    output = L.get_output(merge_layer,x)
+    #output = L.get_output(conv_layer_1,x)
 
-    f = theano.function([x],output)
-    x_test = np.array([[0, 2,3]]).astype('int32')
-    print f(x_test)
+    #f = theano.function([x],output)
+
+    word = unicode("Tat")
+    word_index  = np.array([])
+
+    #print word_index
+
+    for char in word:
+        if char in char_index_lookup:
+            word_index = np.append(word_index, char_index_lookup[char])
+
+    #x_test = np.array([word_index]).astype('int32')
+    #print f(x_test)
+
     return merge_layer
 
 
