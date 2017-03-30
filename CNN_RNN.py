551bae768e80f3a656b8963c8d62f78dd9b62a75
Hoediur <simonstilz@googlemail.com>
2017-03-30 03:05:28 +0200

    code cleanup

--- CNN_RNN.py
+++ CNN_RNN.py
@@ -131,7 +131,7 @@ def create_tag_mappings(unique_tags):
     return tag_to_index, tag_to_vector
 
 # Number of epochs to train the net
-NUM_EPOCHS = 100
+NUM_EPOCHS = 500
 
 # Number of units in the hidden (recurrent) layer
 N_HIDDEN = 10
@@ -143,51 +143,25 @@ max_sentence = 0
 
 def build_network(W,number_unique_tags,longest_word,longest_sentence, input_var=None):
     print("Building network ...")
-    # shape = batch size, longest sent
-    #l_in = lasagne.layers.InputLayer(shape=(None, longest_sent), input_var=input_var)
-    # l_em = lasagne.layers.EmbeddingLayer(l_in, input_size=len(unique_tokens), output_size=100, W=W)
-    #l_em = lasagne.layers.EmbeddingLayer(l_in, input_size=len(unique_tokens), output_size=100,  W = lasagne.init.Uniform(0.1))
-    # l_ex = lasagne.layers.ExpressionLayer(l_in, lambda X: X, output_shape='auto')
 
-    values = np.array(np.random.randint(0,103,(1,longest_sentence,longest_word)))
-
-    input_var.tag.test_value = values
-    #number sentences x words x characters
     input_layer = L.InputLayer((None,longest_sentence,longest_word), input_var=input_var)
 
     embed_layer = L.EmbeddingLayer(input_layer, input_size=103,output_size=101, W=W)
-    print "EMBED", L.get_output(embed_layer).tag.test_value.shape
+
     reshape_embed = L.reshape(embed_layer,(-1,longest_word,101))
-    print "reshap embed", L.get_output(reshape_embed).tag.test_value.shape
+
     conv_layer_1 = L.Conv1DLayer(reshape_embed, longest_word, 2)
     conv_layer_2 = L.Conv1DLayer(reshape_embed, longest_word, 3)
-    print "TEST"
-    print "Convolution Layer 1", L.get_output(conv_layer_1).tag.test_value.shape
-    print "Convolution Layer 2", L.get_output(conv_layer_2).tag.test_value.shape
 
     pool_layer_1 = L.MaxPool1DLayer(conv_layer_1, pool_size=longest_word-1)
     pool_layer_2 = L.MaxPool1DLayer(conv_layer_2, pool_size=longest_word-2)
 
-
-    print "OUTPUT POOL1", L.get_output(pool_layer_1).tag.test_value.shape
-    print "OUTPUT POOL2",L.get_output(pool_layer_2).tag.test_value.shape
-
     merge_layer = L.ConcatLayer([pool_layer_1, pool_layer_2], 1)
-
     flatten_merge = L.flatten(merge_layer, 2)
-    print "longest sent",longest_sentence
     reshape_merge = L.reshape(flatten_merge, (-1,longest_sentence,int(longest_word*2)))
-    print "OUTPUT RESHAPEMERGE", L.get_output(reshape_merge).tag.test_value.shape
-
-
 
     l_re = lasagne.layers.RecurrentLayer(reshape_merge, N_HIDDEN, nonlinearity=lasagne.nonlinearities.sigmoid, mask_input=None)
-    print "OUTPUT RECURRENT", L.get_output(l_re).tag.test_value.shape
-
-    #l_out = lasagne.layers.DenseLayer(l_re, len(unique_tags), nonlinearity=lasagne.nonlinearities.softmax)
-
     l_out = lasagne.layers.DenseLayer(l_re, number_unique_tags, nonlinearity=lasagne.nonlinearities.softmax)
-    print "OUTPUT", L.get_output(l_out).tag.test_value.shape
 
     print "DONE BUILDING NETWORK"
     return l_out
@@ -202,7 +176,6 @@ def build_network(W,number_unique_tags,longest_word,longest_sentence, input_var=
 # it will not return the last (remaining) mini-batch.
 
 def iterate_minibatches(inputs, targets, batchsize=1, shuffle=False):
-    #assert len(inputs) == len(targets)
 
     if shuffle:
         indices = np.arange(len(inputs))
@@ -230,9 +203,6 @@ def create_word_index_vectors(corpus,max_length):
             if n % 10000 == 0:
                 print n
             word_array = []
-
-            #if word == "pad_value_rnn":
-             #   break
 
             for char in unicode(word,"utf-8"):
 
@@ -272,11 +242,6 @@ def create_tag_vectors(corpus_tag_sequence,tag_vector_mappings):
             else:
                 result_test.append(tag_vector_mappings[tag].tolist())
 
-
-            #if result is None:
-            #    result = np.array([tag_vector_mappings[tag]])
-            #else:
-             #  result = np.append(result, [np.array(tag_vector_mappings[tag])], axis=0)
     result = np.array(result_test)
     print result
     return result
@@ -285,86 +250,37 @@ def create_tag_vectors(corpus_tag_sequence,tag_vector_mappings):
 def main():
     max_word_length = 55 #magicNumber
 
-    #input_var = T.lmatrix('inputs')
     input_var = T.tensor3(name="input", dtype='int64')
-    # target_var = T.dtensor3('targets')
     target_var = T.dmatrix('targets')
 
     W = create_char_embedding_matrix()
 
     # Load the dataset
     print "Loading data..."
-    #build cnn
 
     sents_train, tags_train, unique_tokens_train, unique_tags_train, longest_sent_train = prepare_sents(train_corpus)
-    print "longest_sent",longest_sent_train
-    print "Unique tags",len(unique_tags_train)
-    print "# sents", len(unique_tags_train)
-    print "Unique tags", len(unique_tags_train)
-    print "Unique tags", len(sents_train)
 
     token_mappings = create_token_mappings(unique_tokens_train)
     tag_index_mappings, tag_vector_mappings = create_tag_mappings(unique_tags_train)
-
-    print np.array(sents_train).shape
 
     pad_sent(sents_train, tags_train, longest_sent_train)
 
     X_train = create_word_index_vectors(sents_train,max_word_length)
     y_train = create_tag_vectors(tags_train,tag_vector_mappings)
 
-    print "TEST",X_train.shape
-
     sents_val, tags_val, unique_tokens_val, unique_tags_val, longest_sent_val = prepare_sents(val_corpus)
-    print "EVAL"
 
     pad_sent(sents_val, tags_val, longest_sent_train)
-
-
     X_val = create_word_index_vectors(sents_val, max_word_length)
     y_val = create_tag_vectors(tags_val, tag_vector_mappings)
-    #X_val = np.asarray([[[(char_index_table[char] if char in char_index_table else char_index_table["unknown"])for char in unicode(w,"utf-8")]for w in sent] for sent in sents_val])
-    #y_val = np.asarray([[tag_vector_mappings[t] for t in sent_tags] for sent_tags in tags_val])
-    #y_val = np.asarray([[6, 4, 4, 3, 0, 1, 8, 2, 6], [2, 9, 1, 2, 5, 2, 7, 6, 0]])
-
-
-    # y_val = np.asarray(
-    # [[[0., 0., 0., 0., 0., 0., 1., 0., 0., 0.], [0., 0., 0., 0., 1., 0., 0., 0., 0., 0.],
-    #   [0., 0., 0., 0., 1., 0., 0., 0., 0., 0.], [0., 0., 0., 1., 0., 0., 0., 0., 0., 0.],
-    #   [1., 0., 0., 0., 0., 0., 0., 0., 0., 0.], [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
-    #   [0., 0., 0., 0., 0., 0., 0., 0., 1., 0.], [0., 0., 1., 0., 0., 0., 0., 0., 0., 0.],
-    #   [0., 0., 0., 0., 0., 0., 1., 0., 0., 0.]],
-    #  [[0., 0., 1., 0., 0., 0., 0., 0., 0., 0.], [0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
-    #         [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.], [0., 0., 1., 0., 0., 0., 0., 0., 0., 0.],
-    #         [0., 0., 0., 0., 0., 1., 0., 0., 0., 0.], [0., 0., 1., 0., 0., 0., 0., 0., 0., 0.],
-    #         [0., 0., 0., 0., 0., 0., 0., 1., 0., 0.], [0., 0., 0., 0., 0., 0., 1., 0., 0., 0.],
-    #         [0., 0., 0., 0., 0., 0., 1., 0., 0., 0.]]])
 
     sents_test, tags_test, unique_tokens_test, unique_tags_test, longest_sent_test = prepare_sents(test_corpus)
-    print "TEST"
 
     pad_sent(sents_test, tags_test, longest_sent_train)
     X_test = create_word_index_vectors(sents_test, max_word_length)
     y_test = create_tag_vectors(tags_test, tag_vector_mappings)
 
-    #X_test = np.asarray([[[(char_index_table[char] if char in char_index_table else char_index_table["unknown"])for char in unicode(w,"utf-8")]for w in sent] for sent in sents_test])
-    #y_test = np.asarray([[tag_vector_mappings[t] for t in sent_tags] for sent_tags in tags_test])
-
-    #y_test = np.asarray([[6, 4, 4, 3, 0, 1, 8, 2, 6], [2, 9, 1, 2, 5, 2, 7, 6, 0]])
-
-
-
-    # # gensim 2D NumPy matrix
-    # model = gensim.models.Word2Vec(sents_train, min_count=1)
-    # # shape = (len(unique_tokens), 100)
-    # W = model.syn0.astype("float64")
-    # # print W.shape
-    print
-    print "X_SHAPE", X_train.shape[1]
-    print X_train.shape[1]
-    print X_test.shape[1]
-    print X_val.shape[1]
-    network = build_network(W,len(unique_tags_train),max_word_length,int(X_test.shape[1]), input_var)
+    network = build_network(W,len(unique_tags_train),max_word_length,int(max(X_train.shape[1],X_test.shape[1],X_val.shape[1])), input_var)
 
     # Create a loss expression for training, i.e., a scalar objective we want
     # to minimize:
@@ -375,13 +291,14 @@ def main():
     # Create update expressions for training, i.e., how to modify the
     # parameters at each training step. Here, we'll use Stochastic Gradient
     # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
-    print "unique tags",len(unique_tags_train)
+
     params = lasagne.layers.get_all_params(network, trainable=True)
     updates = lasagne.updates.nesterov_momentum(
         loss, params, learning_rate=0.0001, momentum=0.9)
 
-    updates = lasagne.updates.sgd(loss,params,learning_rate=0.0001)
-    print "PARAMS"
+    #updates = lasagne.updates.sgd(loss,params,learning_rate=0.0001)
+    #updates = lasagne.updates.adadelta(loss,params, learning_rate=0.0001)
+
     # Create a loss expression for validation/testing. The crucial difference
     # here is that we do a deterministic forward pass through the network,
     # disabling dropout layers.
@@ -389,7 +306,7 @@ def main():
     test_prediction = lasagne.layers.get_output(network, deterministic=True)
     test_loss = lasagne.objectives.categorical_crossentropy(test_prediction, target_var)
     test_loss = test_loss.mean()
-    # As a bonus, also create an expression for the classification accuracy:
+    # we also create an expression for the classification accuracy:
     test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=0), target_var),
                       dtype=theano.config.floatX)
 
@@ -400,8 +317,7 @@ def main():
     print "training"
     # Compile a second function computing the validation loss and accuracy:
     val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
-    print "value"
-    # Finally, launch the training loop.
+
     print("Starting training...")
     # We iterate over epochs:
     for epoch in range(NUM_EPOCHS):
